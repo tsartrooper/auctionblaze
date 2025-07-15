cbf27c4f771461e180eef7091a484188dd68dc0d
tsartrooper <pratmane00007@gmail.com>
2025-07-15 15:42:29 +0530

    fix: BrowserRouter

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'; // Optional if you're using React Router
 import '../../styles/globals.css';
+import { NavLink } from 'react-router-dom';
 
 
 export default function Sidebar() {
+  console.log("inside sidebar");
   return (
     <aside className="w-64 h-screen bg-white border-r border-gray-200 p-6 hidden md:block">
       <div className="text-2xl font-bold text-gray-800 mb-8">AuctionBlaze</div>
-      <nav className="nav flex-col space-y-2">
+      <nav className="flex flex-col space-y-2">
         <NavLink
           to="/"
           className={({ isActive }) =>
